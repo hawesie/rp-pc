51d8c91c399e245fc3f4ae80f8a2d8ca5e28eee0
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2016-02-14 21:56:45 +0000

    Added simulated move example

--- src/rp/robotics/example/MovableSimExample.java
+++ src/rp/robotics/example/MovableSimExample.java
@@ -1,24 +1,22 @@
 package rp.robotics.example;
 
+import rp.robotics.simulation.Rotate;
+
 import javax.swing.JFrame;
 
-import lejos.robotics.RangeFinder;
+import lejos.robotics.navigation.Pose;
 import rp.robotics.MobileRobotWrapper;
-import rp.robotics.control.RandomGridWalk;
-import rp.robotics.mapping.GridMap;
-import rp.robotics.navigation.GridPose;
-import rp.robotics.navigation.Heading;
+import rp.robotics.simulation.Drive;
 import rp.robotics.simulation.MapBasedSimulation;
 import rp.robotics.simulation.MovableRobot;
 import rp.robotics.simulation.SimulatedRobots;
 import rp.robotics.testing.TestMaps;
-import rp.robotics.visualisation.GridMapVisualisation;
 import rp.robotics.visualisation.KillMeNow;
 import rp.robotics.visualisation.MapVisualisationComponent;
 
 /**
- * An example to get you started. Copy this to your own project, rename it and
- * go.
+ * An example of the underlying move simulation which does not simulate the
+ * motors.
  * 
  * @author Nick Hawes
  *
@@ -31,44 +29,22 @@ public class MovableSimExample {
 		// with a GUI.
 		MapBasedSimulation sim = new MapBasedSimulation(TestMaps.EMPTY_8_x_6);
 
-		// Grid junction numbers
-		int xJunctions = 50;
-		int yJunctions = 21;
+		MobileRobotWrapper<MovableRobot> wrapper = sim.addRobot(SimulatedRobots
+				.makeConfiguration(false, false), new Pose(3, 3, 0));
 
-		float junctionSeparation = 0.40f;
-
-		float xInset = 0.4f;
-		float yInset = 0.41f;
-
-		GridMap gridMap = new GridMap(xJunctions, yJunctions, xInset, yInset,
-				junctionSeparation, sim.getMap());
-
-		// Add a robot of a given configuration to the simulation. The return
-		// value is the object you can use to control the robot. //
-
-		for (int i = 0; i < 5; i++) {
-			// Starting point on the grid
-			GridPose gridStart = new GridPose(8, 8, Heading.PLUS_X);
-
-			MobileRobotWrapper<MovableRobot> wrapper = sim.addRobot(
-					SimulatedRobots.makeConfiguration(false, true),
-					gridMap.toPose(gridStart));
-
-			RangeFinder ranger = sim.getRanger(wrapper);
-
-			RandomGridWalk controller = new RandomGridWalk(wrapper.getRobot(),
-					gridMap, gridStart, ranger);
-
-			new Thread(controller).start();
-		}
-
-		GridMapVisualisation viz = new GridMapVisualisation(gridMap,
-				sim.getMap());
-
-		MapVisualisationComponent.populateVisualisation(viz, sim);
+		MapVisualisationComponent viz = MapVisualisationComponent
+				.createFromSimulation(sim);
 
 		// Add the visualisation to a JFrame to display it
 		displayVisualisation(viz);
+
+		wrapper.getRobot().getPilot().executeMove(new Drive(0.3f, 1f));
+		wrapper.getRobot().getPilot().executeMove(new Rotate(45f, 90));
+		wrapper.getRobot().getPilot().executeMove(new Drive(0.3f, 1f));
+		wrapper.getRobot().getPilot().executeMove(new Rotate(45f, -90));
+		wrapper.getRobot().getPilot().executeMove(new Drive(0.3f, 1f));
+		wrapper.getRobot().getPilot().executeMove(new Rotate(45f, 90));
+		wrapper.getRobot().getPilot().executeMove(new Drive(0.3f, 1f));
 
 	}
 
